b81b44b7e09be55c13da06ade82f0f7516aebad8
wakame-tech <kamata1919@gmail.com>
2020-01-26 18:07:13 +0900

    [Add/WIP] ExpansionTile

--- lib/ui/lesson/section_select.dart
+++ lib/ui/lesson/section_select.dart
@@ -3,40 +3,105 @@ import 'package:flutter/cupertino.dart';
 
 // TODO hide root navbar
 
-class SectionSelectPage extends StatefulWidget {
-  @override
-  _SectionSelectPageState createState() => _SectionSelectPageState();
+class Section {
+  bool isExpanded = false;
+  String title;
+  List<String> phrases;
+
+  Section(this.title, this.phrases, {this.isExpanded: false});
 }
 
-class _SectionSelectPageState extends State<SectionSelectPage>
-    with SingleTickerProviderStateMixin {
-  final _scaffoldKey = GlobalKey<ScaffoldState>();
+class LessonListView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return LessonListViewState();
+  }
+}
+
+// TODO ExpansionTile
+ExpansionTile _createChildPanel(Section section) {
+  return ExpansionTile(
+    title: Container(
+      padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Padding(
+              child: Text(section.title,
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),),
+              padding: EdgeInsets.only(right: 10.0),
+            ),
+            Padding(
+              child: Text(
+                "未クリア", style: TextStyle(fontSize: 18.0, color: Colors.red),),
+              padding: EdgeInsets.only(right: 10.0),
+            ),
+            RaisedButton(
+              child: Text("Start",
+                  style: TextStyle(color: Colors.white, fontSize: 20.0)),
+              color: Colors.orange,
+              shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(5.0),
+              ),
+              onPressed: () {},
+            )
+          ],
+        ),
+    ),
+    children: <Widget>[
+        ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            return Text('$index');
+          },
+          itemCount: 10,
+          // solution for solve assertion list-view <https://stackoverflow.com/questions/52023610/getting-horizontal-viewport-was-given-unbounded-height-with-tabbarview-in-flu>
+          shrinkWrap: true,
+        ),
+      ],
+  );
+}
+
+class LessonListViewState extends State<LessonListView> {
+  var _sections = <Section>[];
 
   @override
   void initState() {
-    super.initState();
+    _sections = [
+      Section("Section1", ["1", "2", "3", "4"]),
+      Section("Section2", ["1", "2", "3", "4"]),
+      Section("Section3", ["1", "2", "3", "4"]),
+      Section("Section4", ["1", "2", "3", "4"]),
+      Section("Section5", ["1", "2", "3", "4"]),
+    ];
 
-    // on mounted
-//    WidgetsBinding.instance.addPostFrameCallback((_) => showModalBottomSheet(
-//      context: context,
-//      builder: (context) {
-//        return Container(
-//          height: 400.0,
-//          child: Padding(
-//            padding: EdgeInsets.all(30.0),
-//            child: Text("AAA"),
-//          ),
-//        );
-//      },
-//      shape: RoundedRectangleBorder(
-//        borderRadius: BorderRadius.all(Radius.circular(20.0)),
-//      ),
-//      isDismissible: false,
-//    ));
+    super.initState();
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 20.0),
+            child: ListView(
+              children: <Widget>[
+                ExpansionTile(
+                  children: _sections.map(_createChildPanel).toList(),
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SectionSelectPage extends StatelessWidget {
   Widget _createLessonTabView() {
-    return Text("Lesson");
+    return LessonListView();
   }
 
   Widget _createTestTabView() {
@@ -45,16 +110,7 @@ class _SectionSelectPageState extends State<SectionSelectPage>
 
   final _tabs = <Tab>[Tab(text: "Lesson"), Tab(text: "Test")];
 
-  List<Widget> _createTabBarViews() {
-    return [
-      _createLessonTabView(),
-      _createTestTabView(),
-    ];
-  }
-
   Widget _createUnderlinedTabbedCard() {
-    var _tabBarViews = _createTabBarViews();
-
     return Card(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(40.0),
@@ -79,7 +135,10 @@ class _SectionSelectPageState extends State<SectionSelectPage>
               ),
             ),
             body: TabBarView(
-              children: _tabBarViews,
+              children: [
+                _createLessonTabView(),
+                _createTestTabView(),
+              ],
             ),
           ),
         ),
