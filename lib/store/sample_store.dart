6c62db21b8722239f563747d51767b08174eabe1
wakame-tech <kamata1919@gmail.com>
2020-02-05 19:56:53 +0900

    [Add] local assets test

--- lib/store/sample_store.dart
+++ lib/store/sample_store.dart
@@ -3,35 +3,7 @@
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:wr_app/model/phrase.dart';
-
-const dummyPhraseJson = '''
-{
-  "english": "When is the homework due?",
-  "japanese": "いつ宿題するんだっけ",
-  "favorite": true,
-  "sample": {
-    "type": "conversation",
-    "content": [
-      {
-        "english": "Have you finished the English homework yet?",
-        "japanese": "宿題終わった?",
-        "avatartUrl": ""
-      },
-      {
-        "english": "No I haven't.\\nWhen is the homework due?",
-        "japanese": "いや、終わってないよ\\nいつ宿題提出するんだっけ？",
-        "avatartUrl": ""
-      },
-      {
-        "english": "It's due next Tuesday.",
-        "japanese": "火曜日だよ",
-        "avatartUrl": ""
-      }
-    ]
-  },
-  "advice": "\\"due\\"は「支払い期限のきた〜」や「満期の〜」といった"
-}
-''';
+import 'package:flutter/services.dart' show rootBundle;
 
 class SampleStore with ChangeNotifier {
   Phrase phrase = Phrase(english: '', japanese: '');
@@ -41,14 +13,17 @@ class SampleStore with ChangeNotifier {
   }
 
   void _fetch() async {
-    try {
-      Map phraseMap = json.decode(dummyPhraseJson);
-      phrase = Phrase.fromJson(phraseMap);
-    } catch (e) {
-      print('json decode error');
-      print(e);
-    }
-
-    notifyListeners();
+    final path = 'res/dummy.json';
+    rootBundle.loadString(path).then((data) {
+      print('[Asset Load] $path');
+      return jsonDecode(data);
+    }).then((json) {
+      return Phrase.fromJson(json);
+    }).then((phrase) {
+      this.phrase = phrase;
+      notifyListeners();
+    }).catchError((error) {
+      print(error);
+    });
   }
 }
