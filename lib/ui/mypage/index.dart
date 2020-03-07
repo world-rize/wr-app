e4925f144bf9b39ccfc289d0af42e0f37aae33b7
wakame-tech <kamata1919@gmail.com>
2020-03-07 08:20:44 +0400

    [Add] Settings

--- lib/ui/mypage/index.dart
+++ lib/ui/mypage/index.dart
@@ -1,19 +1,65 @@
 // Copyright © 2020 WorldRIZe. All rights reserved.
 
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
+import 'package:wr_app/env.dart';
+import 'package:wr_app/store/user.dart';
 
+// TODO(wakame-tech): アカウント情報と設定項目を考える
+/// 設定ページ
 class MyPagePage extends StatelessWidget {
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const <Widget>[
-        Text(
-          'マイページ',
-          style: optionStyle,
+    return SettingsList(
+      sections: [
+        SettingsSection(
+          title: 'アカウント',
+          tiles: [
+            SettingsTile(
+              title: 'アカウント名',
+              subtitle: 'ゲスト',
+              leading: Icon(Icons.people),
+              onTap: () {},
+            ),
+            SettingsTile(
+              title: 'プラン',
+              subtitle: 'ノーマル',
+              leading: Icon(Icons.attach_money),
+              onTap: () {},
+            )
+          ],
         ),
+        SettingsSection(
+          title: 'レッスン',
+          tiles: [
+            SettingsTile(
+              title: '発音',
+              subtitle: 'アメリカ',
+              leading: Icon(Icons.speaker_notes),
+              onTap: () {},
+            ),
+            SettingsTile(
+              title: 'カードめくりの間隔',
+              subtitle: '5秒',
+              leading: Icon(Icons.speaker_notes),
+              onTap: () {},
+            )
+          ],
+        ),
+        SettingsSection(
+          title: 'その他',
+          tiles: [
+            SettingsTile(
+              title: 'バージョン',
+              subtitle: Env.version,
+            ),
+            SettingsTile(
+              title: 'クレジット',
+              subtitle: 'ああああ',
+              onTap: () {},
+            )
+          ],
+        )
       ],
     );
   }
