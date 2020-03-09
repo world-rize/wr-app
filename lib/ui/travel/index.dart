064baacf2db9a3e93881bfa8d219f981bd7f020f
wakame-tech <kamata1919@gmail.com>
2020-03-09 20:31:00 +0400

    [Add] agency, column, travel top page mock

--- lib/ui/travel/index.dart
+++ lib/ui/travel/index.dart
@@ -2,19 +2,58 @@
 
 import 'package:flutter/material.dart';
 
+/// `旅行` ページのトップ
+///
 class TravelPage extends StatelessWidget {
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const _headLineStyle = TextStyle(
+    fontSize: 30,
+    color: Colors.black54,
+    fontWeight: FontWeight.bold,
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const <Widget>[
-        Text(
-          '旅行',
-          style: optionStyle,
-        ),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const <Widget>[
+          // map
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'Map',
+              style: _headLineStyle,
+            ),
+          ),
+          Placeholder(
+            fallbackHeight: 150,
+          ),
+
+          // 今月のおすすめ
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              '今月のおすすめ',
+              style: _headLineStyle,
+            ),
+          ),
+          Placeholder(
+            fallbackHeight: 200,
+          ),
+
+          // あなたにおすすめ
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'あなたにおすすめ',
+              style: _headLineStyle,
+            ),
+          ),
+          Placeholder(
+            fallbackHeight: 200,
+          ),
+        ],
+      ),
     );
   }
 }
