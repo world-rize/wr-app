9ef7506c4c82ad80d962b302a66f535c51528b52
kamata daiki <4617023@ed.tus.ac.jp>
2020-03-16 14:33:22 +0900

    [Chore] dart 2.1 -> 2.3, version -> alpha-M02

--- lib/env.dart
+++ lib/env.dart
@@ -3,6 +3,6 @@
 /// 定数等
 class Env {
   static const appName = 'WR APP';
-  static const version = '0.1.0-dev';
+  static const version = 'alpha-M02';
   static const authors = <String>['wakame-tech', 'yoshiki301'];
 }
