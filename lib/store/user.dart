75036c1881d951c848b65322f7f387f199506682
wakame-tech <kamata1919@gmail.com>
2020-04-12 11:42:36 +0900

    [Chore] cloud functions test

--- lib/store/user.dart
+++ lib/store/user.dart
@@ -1,9 +1,11 @@
 // Copyright © 2020 WorldRIZe. All rights reserved.
 
+import 'package:logger/logger.dart';
 import 'package:flutter/foundation.dart';
 import 'package:wr_app/model/phrase.dart';
 import 'package:wr_app/store/masterdata.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 
 /// FireStore Auth
 final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -30,14 +32,40 @@ class UserStore with ChangeNotifier {
 
   /// Firebase Auth にログイン
   Future<void> signIn() async {
+    final logger = Logger(
+      printer: SimplePrinter()..useColor = true,
+    );
+
     final _result = await _auth.signInAnonymously();
-    print('anonymous sign in');
+
+    logger.d('[UserStore#signIn] anonymous sign in');
+
     _user = _result.user;
+
+    await callTestApi();
   }
 
   /// ゲストユーザーかどうか
   bool _isGuest() {
     return _user.isAnonymous;
+  }
+
+  Future<void> callTestApi() async {
+    final callable = CloudFunctions.instance
+        .getHttpsCallable(functionName: 'test')
+          ..timeout = const Duration(seconds: 10);
+
+    print(callable);
+
+    try {
+      final result = await callable.call({
+        'hoge': 'aaa',
+      });
+
+      print(result.data);
+    } on CloudFunctionsException catch (e) {
+      print(e);
+    }
   }
 
   /// 名前を取得
