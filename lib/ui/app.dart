dc826381ba557efbbe10eef6bc9560c4a3f590e1
wakame-tech <kamata1919@gmail.com>
2020-05-02 03:52:35 +0900

    [Add] intro Firebase Analytics

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,7 +1,9 @@
 // Copyright © 2020 WorldRIZe. All rights reserved.
 
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 //import 'package:wr_app/ui/onboarding_page.dart';
 import 'package:wr_app/ui/root_view.dart';
 import 'package:wr_app/theme.dart';
@@ -16,8 +18,11 @@ class WRApp extends StatefulWidget {
 class WRAppState extends State<WRApp> {
   @override
   Widget build(BuildContext context) {
+    final observer = Provider.of<FirebaseAnalyticsObserver>(context);
+
     return MaterialApp(
       theme: wrThemeData,
+      navigatorObservers: <NavigatorObserver>[observer],
       home: RootView(),
     );
   }
