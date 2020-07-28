5fc26c3542dcb8b426dbe08c1486dbe36543b844
wakame-tech <kamata1919@gmail.com>
2020-07-28 15:28:49 +0900

    :sparkles: phrases management API
    # ==================== Emojis ====================
    # 🎉  :tada: 初めてのコミット（Initial Commit）
    # 🔖  :bookmark: バージョンタグ（Version Tag）
    # ✨  :sparkles: 新機能（New Feature）
    # 🐛  :bug: バグ修正（Bagfix）
    # ♻️  :recycle: リファクタリング(Refactoring)
    # 📚  :books: ドキュメント（Documentation）
    # 🎨  :art: デザインUI/UX(Accessibility)
    # 🐎  :horse: パフォーマンス（Performance）
    # 🔧  :wrench: ツール（Tooling）
    # 🚨  :rotating_light: テスト（Tests）
    # 💩  :hankey: 非推奨追加（Deprecation）
    # 🗑️  :wastebasket: 削除（Removal）
    # 🚧  :construction: WIP(Work In Progress)
    
    # ==================== Format ====================
    # :emoji: Subject
    #
    # Commit body...

--- scripts/worldrize.py
+++ scripts/worldrize.py
@@ -2,35 +2,42 @@
 WorldRIZe CLI
 """
 import os
+import re
 import json
 from pathlib import Path
 import chalk
 from tqdm import tqdm
 import shutil
 import requests
+import string
 import fire
 from dotenv import load_dotenv
-import urllib.parse
 
 pwd = Path(__file__).resolve().parent
 env = pwd.parent / '.env/.env'
 load_dotenv(env)
-root = pwd.parent / 'assets'
-voices_path = pwd / 'voices'
 green, red = chalk.Chalk('green'), chalk.Chalk('red')
 
 def success(*args):
     print(f'{green("✔")} ', *args)
 
+def info(*args):
+    print(f'🔖 ', *args)
+
 def error(*args):
     print(f'{red("✗")} ', *args)
 
-def generate_phrase_voices(phrase: object):
+def strip_brs(txt: str):
+    lines = txt.split('\n')
+    lines = [l for l in lines if l != '\n']
+    return '\n'.join(lines).strip()
+
+def generate_phrase_voices(phrase: object, out_dir: Path):
     # kp, 1, 2, 3
     base_id = f'{phrase["meta"]["lessonId"]}_{phrase["meta"]["phraseId"]}'
     voices = []
     for language in ['en-us', 'en-au', 'en-uk']:
-        path = voices_path / f'{base_id}_kp_{language}.mp3'
+        path = out_dir / f'{base_id}_kp_{language}.mp3'
         voices.append({
             'path': path,
             'gender_voice': 'male',
@@ -39,7 +46,7 @@ def generate_phrase_voices(phrase: object):
         })
     for i, example in enumerate(phrase['example']['value'], start=1):
         for language in ['en-us', 'en-au', 'en-uk']:
-            path = voices_path / f'{base_id}_{i}_{language}.mp3'
+            path = out_dir / f'{base_id}_{i}_{language}.mp3'
             voices.append({
                 'path': path,
                 'gender_voice': 'male' if i % 2 == 0 else 'female',
@@ -70,7 +77,11 @@ def call_api(access_key: str, text: str, gender_voice: str, language: str, out_p
     if gender_voice not in ['male', 'female']:
         raise Exception('invalid gender_type')
     # ' ' -> '+'
-    text = text.replace(' ', ' ').replace('’', '\'')
+    text = text \
+        .replace(' ', ' ') \
+        .replace('’', '\'') \
+        .replace('（', '') \
+        .replace('）', '')
 
     data = {
         'text': text,
@@ -83,13 +94,15 @@ def call_api(access_key: str, text: str, gender_voice: str, language: str, out_p
     }
     # res: {"message":"Your audio has been created!","audio_src":"https:\/\/getwoord.s3.amazonaws.com\/4273352455515882618255eaaf3c1cbdbe0.55443890.mp3","error":false}
     res = requests.post('https://www.getwoord.com/api/convert', headers=headers, data=data)
-    
     if res.status_code != 200:
-        raise Exception(res.text)
+        error(f'{data}')
+        error(f'{res.status_code} {res.json()["message"]}')
+        raise Exception()
 
     res = res.json()
     if res['error']:
-        raise Exception(res['error'])
+        error(f'Error {res["error"]}')
+        raise Exception()
 
     audio_src = res['audio_src']
 
@@ -99,15 +112,162 @@ def call_api(access_key: str, text: str, gender_voice: str, language: str, out_p
         with open(out_path, 'wb') as f:
             shutil.copyfileobj(res.raw, f)
 
+class PhrasesParser(object):
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+        self.lessons = []
+        self.phrases = []
+
+    def _parse_phrase(self, phrase_txt: str, lesson_id: str, phrase_id: str):
+        def create_assets(index: str):
+            return {
+                'voice': { 
+                    locale: f'voice/{lesson_id}_{phrase_id}_{index}_{locale}.mp3' for locale in ['en-us', 'en-au', 'en-uk']
+                }
+            }
+
+        bold_pattern = re.compile(r"[\(（](.*?)[\)）]")
+        content = strip_brs(phrase_txt)
+        
+        try:
+            en1, ja1, en2, ja2, en3, ja3, *advice = content.split('\n')
+            jas = [ja1, ja2, ja3]
+            ens = [en1, en2, en3]
+            advice = strip_brs('\n'.join(advice))
+            example_value = []
+            for i, (ja, en) in enumerate(zip(jas, ens), start=1):
+                example_value.append({
+                    'text': {
+                        'en': en,
+                        'ja': ja,
+                    },
+                    'assets': create_assets(i),
+                })
+
+            title_ja, title_en = '', ''
+            for ja in jas:
+                bold = re.search(bold_pattern, ja)
+                if bold is not None:
+                    title_ja = bold.group(1)
+                    break
+            else:
+                if self.verbose: error(f'Warning {lesson_id} {phrase_id} title(ja) is empty')
+                title_ja = jas[1]
+
+            for en in ens:
+                bold = re.search(bold_pattern, en)
+                if bold is not None:
+                    title_en = bold.group(1)
+                    break
+            else:
+                if self.verbose: error(f'Warning {lesson_id} {phrase_id} title(en) is empty')
+                title_en = ens[1]
+
+            phrase_json = {
+                'id': str(phrase_id).zfill(4),
+                'title': {
+                    'en': title_en,
+                    'ja': title_ja,
+                },
+                'meta': {
+                    'lessonId': lesson_id,
+                    'phraseId': str(phrase_id),
+                },
+                # key phrase
+                'assets': {
+                    'voice': {
+                        locale: f'voice/{lesson_id}_{phrase_id}_kp_{locale}.mp3' for locale in ['en-us', 'en-au', 'en-uk']
+                    },
+                },
+                'advice': {
+                    'ja': advice.strip()
+                },
+                'example': {
+                    'value': example_value,
+                }
+            }
+
+            self.phrases.append(phrase_json)
+        except Exception as e:
+            error(f'Warning {lesson_id} {phrase_id} invalid')
+            error('\t', e)
+
+    def _parse_lesson(self, lesson_txt: str):
+        lesson_txt = strip_brs(lesson_txt)
+        # phrases[0]: title
+        # phrases[1..]: phrases contents
+        header, *phrases = re.split(r'^\d+\.', lesson_txt, flags=re.MULTILINE)
+        title = strip_brs(header)
+        # ex: 'Social Media' -> 'social'
+        lesson_id = title.split(' ')[0].strip(string.punctuation).lower()
+
+        for phrase_id, phrase_txt in enumerate(phrases, start=1):
+            self._parse_phrase(phrase_txt, lesson_id=lesson_id, phrase_id=str(phrase_id))
+
+        lesson_json = {
+            'id': lesson_id,
+            'title': {
+                'en': title,
+                'ja': title,
+            },
+            'assets': {
+                'img': {
+                    'thumbnail': f'assets/thumbnails/{lesson_id}.png'
+                }
+            }
+        }
+        self.lessons.append(lesson_json)
+
+    def parse(self, src: str):
+        # lessons[0]: legends
+        # lessons[1..]: lessons contents
+        _, *lessons = src.split('#')
+        for lesson in lessons:
+            self._parse_lesson(lesson)
+
+        lessons_count = len(self.lessons)
+        phrases_count = len(self.phrases)
+        success(f'Parsed {lessons_count} Lessons, {phrases_count} Phrases.') 
+        return self.lessons, self.phrases
+
 class Cli(object):
-    def phrases(self):
-        """ Generate Phrases """
+    def __init__(self):
+        self.root = pwd.parent / 'assets'
+        self.voices_path = pwd / 'voices'
+        self.lessons_txt_path = self.root / 'phrases/v1.md'
+        self.lessons_json_path = self.root / 'lessons.json'
+        self.phrases_json_path  = self.root / 'phrases.json'
+
+    def resources(self):
+        """ Download thumbnails from Google Drive """
+        # TODO
         pass
+
+    def phrases(self, verbose=False, dry_run=False):
+        """ Generate Phrases """
+        src = self.lessons_txt_path.read_text()
+
+        parser = PhrasesParser(verbose=verbose)
+        lessons_json, phrases_json = parser.parse(src)
+
+        if self.lessons_json_path.exists():
+            info(f'{self.lessons_json_path} is already exists')
+
+        lessons = json.dumps(lessons_json, ensure_ascii=False, indent=2)
+        phrases = json.dumps(phrases_json, ensure_ascii=False, indent=2)
+        if not dry_run:
+            self.lessons_json_path.write_text(lessons)
+            self.phrases_json_path.write_text(phrases)
+        else:
+            info('Dry Run Mode')
+
+        success(f'Generated {self.lessons_json_path}')
+        success(f'Generated {self.phrases_json_path}')
 
     def voices(self):
         """ Generate voices"""
         # read phrases.json
-        phrases_json_path = root / 'phrases.json'
+        phrases_json_path = self.root / 'phrases.json'
         if not phrases_json_path.exists():
             error('Lessons JSON File')
             exit()
@@ -115,20 +275,15 @@ class Cli(object):
         with open(phrases_json_path, 'r') as j:
             phrases = json.load(j)
         
-        start, stop = 0, 1
-        for phrase in phrases[start:stop]:
-            generate_phrase_voices(phrase)
+        for phrase in phrases:
+            generate_phrase_voices(phrase, out_dir=self.voices_path)
 
     def info(self, verbose=False):
         """ Show infomation """
-        lessons_txt_path = root / 'phrases/v1.md'
-        (success if lessons_txt_path.exists() else error)('Phrases Markdown File')
-
-        lessons_json_path = root / 'lessons.json'
-        (success if lessons_json_path.exists() else error)('Lessons JSON File')
-        
-        phrases_json_path  = root / 'phrases.json'
-        (success if phrases_json_path.exists() else error)('Phrases JSON File')
+        (success if self.lessons_txt_path.exists() else error)('Phrases Markdown File')
+        (success if self.lessons_json_path.exists() else error)('Lessons JSON File')
+        (success if self.phrases_json_path.exists() else error)('Phrases JSON File')
+        (success if self.voices_path.exists() else error)('Voices Dir')
 
 if __name__ == '__main__':
     fire.Fire(Cli)
