e4e9e5498b32b04fa26294086b3404889bedfaeb
wakame-tech <kamata1919@gmail.com>
2020-08-22 11:29:17 +0900

    🚨 driver test sample
    # 🎉🔖✨🐛♻️📚🎨🔧🚨🗑️🚧

---
+++ test_driver/app.dart
@@ -0,0 +1,9 @@
+// Copyright © 2020 WorldRIZe. All rights reserved.
+
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:wr_app/main_development.dart' as app;
+
+void main() {
+  enableFlutterDriverExtension();
+  app.main();
+}
