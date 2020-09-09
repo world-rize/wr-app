f2091d9aeed216c3b243ea98c8a84f4e3ef748db
wakame-tech <kamata1919@gmail.com>
2020-09-09 17:57:08 +0900

    🐛 webview # 🎉🔖✨🐛♻️📚🎨🔧🚨🗑️🚧

--- lib/presentation/article/pages/article_webview_page.dart
+++ lib/presentation/article/pages/article_webview_page.dart
@@ -1,82 +1,29 @@
 // Copyright © 2020 WorldRIZe. All rights reserved.
 
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:wr_app/domain/article/index.dart';
 
-class MyInAppBrowser extends InAppBrowser {
-  @override
-  Future onBrowserCreated() async {
-    print("\n\nBrowser Created!\n\n");
-  }
-
-  @override
-  Future onLoadStart(String url) async {
-    print("\n\nStarted $url\n\n");
-  }
-
-  @override
-  Future onLoadStop(String url) async {
-    print("\n\nStopped $url\n\n");
-  }
-
-  @override
-  void onLoadError(String url, int code, String message) {
-    print("Can't load $url.. Error: $message");
-  }
-
-  @override
-  void onProgressChanged(int progress) {
-    print("Progress: $progress");
-  }
-
-  @override
-  void onExit() {
-    print("\n\nBrowser closed!\n\n");
-  }
-
-  @override
-  Future<ShouldOverrideUrlLoadingAction> shouldOverrideUrlLoading(
-      ShouldOverrideUrlLoadingRequest shouldOverrideUrlLoadingRequest) async {
-    print("\n\nOverride ${shouldOverrideUrlLoadingRequest.url}\n\n");
-    return ShouldOverrideUrlLoadingAction.ALLOW;
-  }
-
-  @override
-  void onLoadResource(LoadedResource response) {
-    print("Started at: " +
-        response.startTime.toString() +
-        "ms ---> duration: " +
-        response.duration.toString() +
-        "ms " +
-        response.url);
-  }
-
-  @override
-  void onConsoleMessage(ConsoleMessage consoleMessage) {
-    print("""
-    console output:
-      message: ${consoleMessage.message}
-      messageLevel: ${consoleMessage.messageLevel.toValue()}
-   """);
-  }
-}
+class MyInAppBrowser extends InAppBrowser {}
 
 class ArticleWebViewPage extends StatefulWidget {
   ArticleWebViewPage({
-    @required this.article,
+    @required this.articleDigest,
   });
 
-  ArticleDigest article;
-  final browser = MyInAppBrowser();
+  ArticleDigest articleDigest;
 
   @override
-  _ArticleWebViewPageState createState() => _ArticleWebViewPageState();
+  _ArticleWebViewPageState createState() =>
+      _ArticleWebViewPageState(articleDigest: articleDigest);
 }
 
 class _ArticleWebViewPageState extends State<ArticleWebViewPage> {
+  _ArticleWebViewPageState({@required this.articleDigest});
+
+  ArticleDigest articleDigest;
+  InAppWebViewController webView;
+
   @override
   void initState() {
     super.initState();
@@ -84,41 +31,51 @@ class _ArticleWebViewPageState extends State<ArticleWebViewPage> {
 
   @override
   Widget build(BuildContext context) {
+    print(articleDigest.fields.toJson());
+
     return Scaffold(
       appBar: AppBar(
           title: Text(
         "InAppBrowser",
       )),
-      body: Center(
+      body: Container(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            RaisedButton(
-              onPressed: null,
-//              onPressed: () async {
-//                await widget.browser.openFile(
-//                  assetFilePath: "assets/index.html",
-//                  options: InAppBrowserClassOptions(
-//                    inAppWebViewGroupOptions: InAppWebViewGroupOptions(
-//                      crossPlatform: InAppWebViewOptions(
-//                        debuggingEnabled: true,
-//                        useShouldOverrideUrlLoading: true,
-//                        useOnLoadResource: true,
-//                      ),
-//                    ),
-//                  ),
-//                );
-//              },
-              child: Text("Open In-App Browser"),
-            ),
-            Container(height: 40),
-            RaisedButton(
-              onPressed: () async {
-                await InAppBrowser.openWithSystemBrowser(
-                    url: "https://flutter.dev/");
-              },
-              child: Text("Open System Browser"),
-            ),
+//            if (articleDigest.fields.url == null)
+//              Placeholder(
+//                fallbackHeight: 200,
+//              )
+//            else
+            Expanded(
+              child: InAppWebView(
+                initialUrl: articleDigest.fields.url ?? 'https://flutter.dev/',
+                //          initialHeaders: {},
+                //          initialOptions: InAppWebViewGroupOptions(
+                //              crossPlatform: InAppWebViewOptions(
+                //                debuggingEnabled: true,
+                //              )
+                //          ),
+                onWebViewCreated: (InAppWebViewController controller) {
+                  webView = controller;
+                },
+                //          onLoadStart: (InAppWebViewController controller, String url) {
+                //            setState(() {
+                //              this.url = url;
+                //            });
+                //          },
+                //          onLoadStop: (InAppWebViewController controller, String url) async {
+                //            setState(() {
+                //              this.url = url;
+                //            });
+                //          },
+                //          onProgressChanged: (InAppWebViewController controller, int progress) {
+                //            setState(() {
+                //              this.progress = progress / 100;
+                //            });
+                //          },
+              ),
+            )
           ],
         ),
       ),
