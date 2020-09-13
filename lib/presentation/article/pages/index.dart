bb4f0b91a235a59baadefae0e5eb4bcd1d693073
wakame-tech <kamata1919@gmail.com>
2020-09-13 10:47:37 +0900

    🐛 article category link
    # 🎉🔖✨🐛♻️📚🎨🔧🚨🗑️🚧

--- lib/presentation/article/pages/index.dart
+++ lib/presentation/article/pages/index.dart
@@ -1,22 +1,25 @@
 // Copyright © 2020 WorldRIZe. All rights reserved.
 
+import 'package:admob_flutter/admob_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:getflutter/colors/gf_color.dart';
 import 'package:getflutter/getflutter.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:wr_app/presentation/article/notifier/article_notifier.dart';
-import 'package:wr_app/presentation/article/pages/category_posts_page.dart';
 import 'package:wr_app/presentation/article/widgets/category_view.dart';
-import 'package:wr_app/ui/widgets/admob_widget.dart';
 import 'package:wr_app/ui/widgets/header1.dart';
+import 'package:wr_app/util/env_keys.dart';
 import 'package:wr_app/util/extensions.dart';
 
 /// Column > index
 class ColumnIndexPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final notifier = Provider.of<ArticleNotifier>(context);
-    final categories = notifier.getCategories();
+    final env = GetIt.I<EnvKeys>();
+    final an = Provider.of<ArticleNotifier>(context);
+    final categories = an.getCategories();
 
     return SingleChildScrollView(
       child: Column(
@@ -27,15 +30,29 @@ class ColumnIndexPage extends StatelessWidget {
           // categories
           ...categories.map(
             (category) => CategoryView(
-              category: category,
-              onTap: (article) => Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => CategoryPosts(category: category),
-                ),
-              ),
-            ),
+                category: category,
+                onTap: (article) async {
+                  if (await canLaunch(category.url)) {
+                    await launch(
+                      category.url,
+                      forceSafariVC: false,
+                      forceWebView: false,
+                    );
+                  }
+
+                  // TODO
+//                  Navigator.of(context).push(
+//                    MaterialPageRoute(
+//                      builder: (_) => CategoryPosts(category: category),
+//                    ),
+//                  );
+                }),
           ),
-          const AdmobBannerWidget(),
+          // admob banner
+          AdmobBanner(
+            adUnitId: env.admobBannerAdUnitId,
+            adSize: AdmobBannerSize.LEADERBOARD,
+          ),
         ],
       ),
     );
